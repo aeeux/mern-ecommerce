2f56b473a5113807f0e60461775ecab1fc691cc3
AeeUX <AndreasEriksen99@hotmail.com>
2021-06-28 14:58:41 +0200

    Create CartItem.js

---
+++ frontend/src/components/CartItem.js
@@ -0,0 +1,11 @@
+import './CartItem.css';
+
+const CartItem = () => {
+    return (
+        <div className="cartitem">
+            <h2>Cart Item</h2>
+        </div>
+    )
+}
+
+export default CartItem
