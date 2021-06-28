62f94f80d34f5606a57ba30c238787b94ebc9713
AeeUX <AndreasEriksen99@hotmail.com>
2021-06-28 07:16:04 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
+    <Router>
       {/* Navbar */}
       {/* SideDrawer */}
       {/* Backdrop */}
-      {/* HomeScreen */}
-      {/* ProductScreen */}
-      {/* CartScreen */}
-    </div>
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
