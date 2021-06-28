9b8baa64d718e794f2512ddfb30c8e6b135718d0
AeeUX <AndreasEriksen99@hotmail.com>
2021-06-28 07:34:42 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,18 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+//Screens
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
+//Components
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <Router>
-      {/* Navbar */}
+      <Navbar />
       {/* SideDrawer */}
       {/* Backdrop */}
       <main>
