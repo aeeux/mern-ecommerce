57d4b705ed538966b0073a5520c4c5332e07d05f
AeeUX <AndreasEriksen99@hotmail.com>
2021-06-28 14:49:45 +0200

    Update ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,40 @@ import './ProductScreen.css';
 const ProductScreen = () => {
     return (
         <div className="productscreen">
-            ProductScreen
+            <div className="productscreen__left">
+                <div className="left__image">
+                <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?
+                    ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" 
+                    alt="product name" />
+                </div>
+                <div className="left__info">
+                    <p className="left__name">Product 1</p>
+                    <p>Price: $499.99</p>
+                    <p>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores perspiciatis voluptatum quisquam modi tempora at.</p>
+                </div>
+            </div>
+            <div className="productscreen__right">
+                <div className="right__info">
+                    <p>
+                        Price: <span>$499.99</span>
+                    </p>
+                    <p>
+                        Status: <span>In Stock</span>
+                    </p>
+                    <p>
+                        Qty
+                        <select>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                    </p>
+                    <p>
+                        <button type="button">Add To Cart</button>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
