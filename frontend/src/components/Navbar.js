d30bd3097e72a437d6e3882187d861c74047e26a
AeeUX <AndreasEriksen99@hotmail.com>
2021-06-28 07:10:13 +0200

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* Logo */}
+            <div className="navbar__logo">
+                <h2>MERN shopping cart</h2>
+            </div>
+            {/* Links */}
+            <ul className="navbar__links">
+
+            </ul>
+
+            {/* Burgermenu */}
+
+        </nav>
+    )
+}
+
+export default Navbar
