9b0a5a39ea41bb901f848e377fb0ca709a36bfe3
AeeUX <AndreasEriksen99@hotmail.com>
2021-06-28 17:25:22 +0200

    Update CartItem.js

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -1,31 +1,35 @@
-import './CartItem.css';
-import { Link } from 'react-router-dom';
+import "./CartItem.css";
+import { Link } from "react-router-dom";
 
-const CartItem = () => {
-    return (
-        <div className="cartitem">
-            <div className="cartitem__image">
-                <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?
-                ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" 
-                alt="product name" />
-            </div>
-            <Link className="cartitem__name" to={`/product/${111}`}>
-                <p>Product 1</p>
-            </Link>
-            <p className="cartitem__price">
-                $499.99
-            </p>
-            <select className="cartitem__select">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-            </select>
-            <button className="cartitem__deleteBtn">
-                <i className="fas fa-trash"></i>
-            </button>
-        </div>
-    )
-}
+const CartItem = ({ item, qtyChangeHandler, removeHandler }) => {
+  return (
+    <div className="cartitem">
+      <div className="cartitem__image">
+        <img src={item.imageUrl} alt={item.name} />
+      </div>
+      <Link to={`/product/${item.product}`} className="cartItem__name">
+        <p>{item.name}</p>
+      </Link>
+      <p className="cartitem__price">${item.price}</p>
+      <select
+        value={item.qty}
+        onChange={(e) => qtyChangeHandler(item.product, e.target.value)}
+        className="cartItem__select"
+      >
+        {[...Array(item.countInStock).keys()].map((x) => (
+          <option key={x + 1} value={x + 1}>
+            {x + 1}
+          </option>
+        ))}
+      </select>
+      <button
+        className="cartItem__deleteBtn"
+        onClick={() => removeHandler(item.product)}
+      >
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+};
 
-export default CartItem
+export default CartItem;
