2cf15832d99a4211e3604213d48e218788506cd6
AeeUX <AndreasEriksen99@hotmail.com>
2021-06-28 16:22:38 +0200

    Update cartActions.js

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -16,5 +16,14 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
         }
     })
 
-    localStorage.setItem('cart', json.stringify(getState().cart))
+    localStorage.setItem('cart', json.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: actionTypes.REMOVE_FROM_CART,
+        payload: id
+    })
+
+    localStorage.setItem('cart', JSON.stringify(getState().cart.cartItems));
 }
