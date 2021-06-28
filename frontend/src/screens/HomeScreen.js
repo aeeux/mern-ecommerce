11de36ade9c7f88714aa494bfa8a48a7c2f4441a
AeeUX <AndreasEriksen99@hotmail.com>
2021-06-28 07:34:32 +0200

    Update HomeScreen.js

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import './HomeScreen.css';
+
+
+const HomeScreen = () => {
+    return (
+        <div className="homescreen">
+            <h2>HomeScreen</h2>
+        </div>
+    )
+}
+
+export default HomeScreen
